c12a35dc4abc180f1c014f5e943dc15aed4c365a
codewithayush7 <codewithayush7@gmail.com>
2026-02-10 23:20:45 +0530

    SOLVING 502 BAD GATEWAY

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,13 +1,21 @@
 import { v2 as cloudinary } from "cloudinary";
 
-const connectCloudinary = async () => {
-  console.log("Cloudinary configured");
+const connectCloudinary = () => {
+  if (
+    !process.env.CLOUDINARY_NAME ||
+    !process.env.CLOUDINARY_API_KEY ||
+    !process.env.CLOUDINARY_SECRET_KEY
+  ) {
+    throw new Error("Cloudinary environment variables missing");
+  }
 
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET_KEY,
   });
+
+  console.log("Cloudinary configured");
 };
 
 export default connectCloudinary;
